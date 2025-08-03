781956b1ebc9305ae77cc7961229540518e179e0
Akhilesh Bashettiwar <akhilesh.bashettiwar@servicenow.com>
2025-08-03 09:57:58 +0530

    Addeed vite project with chakra UI 2.5.1 and react 18.2.0

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import { Button, Box, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={6}>
+      <Heading mb={4}>Hello Chakra + Vite!</Heading>
+      <Button colorScheme="teal">Click Me</Button>
+    </Box>
+  );
+}
+
+export default App;
