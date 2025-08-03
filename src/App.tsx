d4b73f66fd19973ee5c08c350e128c0ab4c6634f
Akhilesh Bashettiwar <akhilesh.bashettiwar@servicenow.com>
2025-08-03 19:51:31 +0530

    build a chakra responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 // src/App.tsx
-import { Button, Box, Heading } from "@chakra-ui/react";
+import { Button, Box, Heading, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box p={6}>
-      <Heading mb={4}>Hello Chakra + Vite!</Heading>
-      <Button colorScheme="teal">Click Me</Button>
-    </Box>
+    <Grid
+      templateAreas={{
+        base: `"nav main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
