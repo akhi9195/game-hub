83f3f09fcfde381acf20741e3f98968ccb9043e0
akhi9195 <akhi9195@gmail.com>
2026-08-10 14:54:22 +0530

    Updated Genre List to show a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,19 @@
+import { List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.genre}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <Text fontSize="lg">{genre.genre}</Text>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
